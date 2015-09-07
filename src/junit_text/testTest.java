efad76213eb67a7af7772f8d816cc393ff2813aa
kumaaaa <1798071219@qq.com>
2015-09-08 02:34:55 +0800

    项目提交

---
+++ src/junit_text/testTest.java
@@ -0,0 +1,16 @@
+package junit_text;
+
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class testTest {
+	private  static text c=new text();
+	@Test
+	public void setUp() throws Exception{
+	}
+	public void testGetsum() {
+		c.getsum(13,2);
+	}
+
+}
---
+++ src/junit_text/text.java
@@ -0,0 +1,9 @@
+package junit_text;
+
+public class text {
+	int a,b,sum;
+	public  void getsum(int a,int b) {
+		sum=a+b;
+		System.out.println("两者之和为:"+sum);
+			}
+}
